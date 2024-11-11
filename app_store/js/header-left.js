eada168838d9006fce9daea4515159502d76206b
朱阿南 <zhuanan@zhuanandeMacBook-Pro.local>
2024-11-11 20:22:11 +0800

    devtool

--- app_store/js/header-left.js
+++ app_store/js/header-left.js
@@ -1,102 +1,125 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const navbarHtml = `
-      
-       <nav class="sidebar">
-        <ul class="nav-list">
-          <li class="nav-item" data-page="tool"> <a href="#" class="nav-link"> <i class="icon briefcase"></i> <span class="link-text">工具</span>  </a>
+document.addEventListener('DOMContentLoaded', function () {
+  const navbarHtml = `<nav class="sidebar">
+    <ul class="nav-list">
+        <li class="nav-item" data-page="tool"> <a href="#" class="nav-link"> <i class="icon briefcase"></i> <span
+                    class="link-text">常用工具</span> </a>
             <ul class="sub-menu">
-              <li data-page="alist"><a href="/app_store/tool/alist">
-                <div class="sub-menu-lef"> <span class="dot alist-color"></span>
-                  <p>Alist <span class="sub-muen-desctie">- 在线网盘管理系统</span></p>
-                </div>
-                </a> </li>
-              <li data-page="affine"><a href="/app_store/tool/affine">
-                <div class="sub-menu-lef"> <span class="dot affine-color"></span>
-                  <p>Affine <span class="sub-muen-desctie">- 开源笔记软件</span></p>
-                </div>
-                </a> </li>
-              <li data-page="Headscale"><a href="/app_store/tool/headscale">
-                <div class="sub-menu-lef"> <span class="dot Headscale-color"></span>
-                  <p>Headscale <span class="sub-muen-desctie">- 构建 WireGuard 网络</span></p>
-                </div>
-                </a> </li>
-              <li data-page="DeeplX"><a href="/app_store/tool/deeplx">
-                <div class="sub-menu-lef"> <span class="dot DeeplX-color"></span>
-                  <p>DeeplX <span class="sub-muen-desctie">- 翻译API</span></p>
-                </div>
-                </a> </li>
-              <li data-page="Vaultwarden"><a href="/app_store/tool/vaultwarden">
-                <div class="sub-menu-lef"> <span class="dot Vaultwarden-color"></span>
-                  <p>Vaultwarden <span class="sub-muen-desctie">- 密码管理器</span></p>
-                </div>
-                </a> </li>
-              <li data-page="kuboard"><a href="/app_store/tool/kuboard">
-                <div class="sub-menu-lef"> <span class="dot kuboard-color"></span>
-                  <p>kuboard <span class="sub-muen-desctie">- Kubernetes管理界面</span></p>
-                </div>
-                </a> </li>
-              <li data-page="blossom"><a href="/app_store/tool/blossom">
-                <div class="sub-menu-lef"> <span class="dot blossom-color"></span>
-                  <p>blossom <span class="sub-muen-desctie">- 笔记软件</span></p>
-                </div>
-                </a> </li>
-              <li data-page="code-server"><a href="/app_store/tool/code-server">
-                <div class="sub-menu-lef"> <span class="dot code-color"></span>
-                  <p>code-server <span class="sub-muen-desctie">- VS-Code</span></p>
-                </div>
-                </a> 
-              </li>
-              <li data-page="ubuntu"><a href="/app_store/tool/ubuntu">
-                <div class="sub-menu-lef"> <span class="dot code-color"></span>
-                  <p>Ubuntu <span class="sub-muen-desctie">- Linux发行版操作系统</span></p>
-                </div>
-                </a> 
-              </li>
-              <li data-page="plane"><a href="/app_store/tool/plane">
-                <div class="sub-menu-lef"> <span class="dot code-color"></span>
-                  <p>Plane <span class="sub-muen-desctie">- AI驱动的开源项目管理系统</span></p>
-                </div>
-                </a> 
-              </li>
-              <li data-page="nexus"><a href="/app_store/tool/nexus">
-                <div class="sub-menu-lef"> <span class="dot code-color"></span>
-                  <p>Nexus <span class="sub-muen-desctie">- Maven库管理器</span></p>
-                </div>
-                </a> 
-              </li>
-              <li data-page="overleaf"><a href="/app_store/tool/overleaf">
-                <div class="sub-menu-lef"> <span class="dot blossom-color"></span>
-                  <p>Overleaf<span class="sub-muen-desctie">-LaTeX编辑器</span></p>
-                </div>
-                </a> 
-              </li>
-              <li data-page="plausible"><a href="/app_store/tool/plausible">
-                <div class="sub-menu-lef"> <span class="dot blossom-color"></span>
-                  <p>Plausible<span class="sub-muen-desctie">-流量分析工具</span></p>
-                </div>
-                </a> 
-              </li>
-              <li data-page="anki"><a href="/app_store/tool/anki">
-                <div class="sub-menu-lef"> <span class="dot blossom-color"></span>
-                  <p>Anki<span class="sub-muen-desctie">-个人化Anki同步服务器</span></p>
-                </div>
-                </a> 
-              </li>
-              <li data-page="frp"><a href="/app_store/tool/frp">
-                <div class="sub-menu-lef"> <span class="dot blossom-color"></span>
-                  <p>Frp<span class="sub-muen-desctie">-反向代理工具</span></p>
-                </div>
-                </a> 
-              </li>
-              <li data-page="crmeb"><a href="/app_store/tool/crmeb">
-                <div class="sub-menu-lef"> <span class="dot blossom-color"></span>
-                  <p>CRMEB<span class="sub-muen-desctie">-在线教育知识付费系统</span></p>
-                </div>
-                </a> 
-              </li>
-      </ul>
-          </li>
-          <li class="nav-item" data-page="AI"> <a href="#" class="nav-link"> <i class="icon briefcase-1"></i> <span class="link-text">AI</span>  </a>
+                <li data-page="alist"><a href="/app_store/tool/alist">
+                        <div class="sub-menu-lef"> <span class="dot alist-color"></span>
+                            <p>Alist <span class="sub-muen-desctie">- 在线网盘管理系统</span></p>
+                        </div>
+                    </a> </li>
+                <li data-page="affine"><a href="/app_store/tool/affine">
+                        <div class="sub-menu-lef"> <span class="dot affine-color"></span>
+                            <p>Affine <span class="sub-muen-desctie">- 开源笔记软件</span></p>
+                        </div>
+                    </a> </li>
+
+                <li data-page="DeeplX"><a href="/app_store/tool/deeplx">
+                        <div class="sub-menu-lef"> <span class="dot DeeplX-color"></span>
+                            <p>DeeplX <span class="sub-muen-desctie">- 翻译API</span></p>
+                        </div>
+                    </a> </li>
+                <li data-page="Vaultwarden"><a href="/app_store/tool/vaultwarden">
+                        <div class="sub-menu-lef"> <span class="dot Vaultwarden-color"></span>
+                            <p>Vaultwarden <span class="sub-muen-desctie">- 密码管理器</span></p>
+                        </div>
+                    </a> </li>
+                <li data-page="blossom"><a href="/app_store/tool/blossom">
+                        <div class="sub-menu-lef"> <span class="dot blossom-color"></span>
+                            <p>blossom <span class="sub-muen-desctie">- 笔记软件</span></p>
+                        </div>
+                    </a> </li>
+                <li data-page="plane"><a href="/app_store/tool/plane">
+                        <div class="sub-menu-lef"> <span class="dot code-color"></span>
+                            <p>Plane <span class="sub-muen-desctie">- AI驱动的开源项目管理系统</span></p>
+                        </div>
+                    </a>
+                </li>
+                <li data-page="overleaf"><a href="/app_store/tool/overleaf">
+                        <div class="sub-menu-lef"> <span class="dot blossom-color"></span>
+                            <p>Overleaf<span class="sub-muen-desctie">-LaTeX编辑器</span></p>
+                        </div>
+                    </a>
+                </li>
+                <li data-page="plausible"><a href="/app_store/tool/plausible">
+                        <div class="sub-menu-lef"> <span class="dot blossom-color"></span>
+                            <p>Plausible<span class="sub-muen-desctie">-流量分析工具</span></p>
+                        </div>
+                    </a>
+                </li>
+                <li data-page="anki"><a href="/app_store/tool/anki">
+                        <div class="sub-menu-lef"> <span class="dot blossom-color"></span>
+                            <p>Anki<span class="sub-muen-desctie">-个人化Anki同步服务器</span></p>
+                        </div>
+                    </a>
+                </li>
+                <li data-page="excalidraw"><a href="/app_store/tool/excalidraw">
+                        <div class="sub-menu-lef"> <span class="dot blossom-color"></span>
+                            <p>Excalidraw<span class="sub-muen-desctie">-白板手绘工具</span></p>
+                        </div>
+                    </a>
+                </li>
+
+
+
+            </ul>
+        </li>
+        <li class="nav-item" data-page="devetool"> <a href="#" class="nav-link"> <i class="icon briefcase-3"></i> <span
+                    class="link-text">开发工具</span> </a>
+            <ul class="sub-menu">
+                <li data-page="ubuntu"><a href="/app_store/devetool/ubuntu">
+                        <div class="sub-menu-lef"> <span class="dot code-color"></span>
+                            <p>Ubuntu <span class="sub-muen-desctie">- Linux发行版操作系统</span></p>
+                        </div>
+                    </a>
+                </li>
+                <li data-page="headscale"><a href="/app_store/devetool/headscale">
+                        <div class="sub-menu-lef"> <span class="dot Headscale-color"></span>
+                            <p>Headscale <span class="sub-muen-desctie">- 构建 WireGuard 网络</span></p>
+                        </div>
+                    </a>
+                </li>
+                <li data-page="kuboard"><a href="/app_store/devetool/kuboard">
+                        <div class="sub-menu-lef"> <span class="dot kuboard-color"></span>
+                            <p>kuboard <span class="sub-muen-desctie">- Kubernetes管理界面</span></p>
+                        </div>
+                    </a>
+                </li>
+                <li data-page="code-server"><a href="/app_store/devetool/code-server">
+                        <div class="sub-menu-lef"> <span class="dot code-color"></span>
+                            <p>code-server <span class="sub-muen-desctie">- VS-Code</span></p>
+                        </div>
+                    </a>
+                </li>
+                <li data-page="frp"><a href="/app_store/devetool/frp">
+                        <div class="sub-menu-lef"> <span class="dot blossom-color"></span>
+                            <p>Frp<span class="sub-muen-desctie">-反向代理工具</span></p>
+                        </div>
+                    </a>
+                </li>
+                <li data-page="nexus"><a href="/app_store/devetool/nexus">
+                        <div class="sub-menu-lef"> <span class="dot code-color"></span>
+                            <p>Nexus <span class="sub-muen-desctie">- Maven库管理器</span></p>
+                        </div>
+                    </a>
+                </li>
+                <li data-page="phpmyadmin"><a href="/app_store/devetool/phpmyadmin">
+                        <div class="sub-menu-lef"> <span class="dot blossom-color"></span>
+                            <p>phpmyadmin<span class="sub-muen-desctie">-MySQL客户端软件</span></p>
+                        </div>
+                    </a>
+                </li>
+                <li data-page="gitea"><a href="/app_store/tool/gitea">
+                        <div class="sub-menu-lef"> <span class="dot blossom-color"></span>
+                            <p>Gitea<span class="sub-muen-desctie">-自托管 Git 服务</span></p>
+                        </div>
+                    </a>
+                </li>
+            </ul>
+        </li>
+         <li class="nav-item" data-page="AI"> <a href="#" class="nav-link"> <i class="icon briefcase-1"></i> <span
+                    class="link-text">AI</span> </a>
             <ul class="sub-menu">
               <li data-page="Chatgpt-next-web"><a href="/app_store/ai/chatgpt-next-web">
                 <div class="sub-menu-lef"> <span class="dot alist-color"></span>
@@ -229,9 +252,9 @@ document.addEventListener('DOMContentLoaded', function() {
     
     `;
 
-    // 获取导航栏容器并插入内容
-    const navContainer = document.getElementById('left-header');
-    if (navContainer) {
-        navContainer.innerHTML = navbarHtml;
-    }
+  // 获取导航栏容器并插入内容
+  const navContainer = document.getElementById('left-header');
+  if (navContainer) {
+    navContainer.innerHTML = navbarHtml;
+  }
 });
