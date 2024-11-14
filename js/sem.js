87332dd9881bc84c41383306d21f5d8f561b1b61
zjy <3161362058@qq.com>
2024-11-14 15:44:15 +0800

    add sem.js

---
+++ js/sem.js
@@ -0,0 +1,37 @@
+document.addEventListener('DOMContentLoaded', function () {
+  console.log('sem is loaded')
+  // 选择所有的 a 标签，而不是仅限于 .login-link
+  var links = document.querySelectorAll('a')
+
+
+  links.forEach(function (link) {
+    link.addEventListener('click', function (e) {
+      e.preventDefault()
+
+      var baseUrl = this.getAttribute('href')
+
+      // 如果链接不存在或是锚点链接，则不处理
+      if (!baseUrl || baseUrl.startsWith('#')) {
+        return
+      }
+
+      var urlParams = new URLSearchParams(window.location.search)
+      var bdVid = urlParams.get('bd_vid')
+      var kValue = urlParams.get('k')
+
+      var newUrl = baseUrl
+      if (bdVid) {
+        newUrl += (baseUrl.includes('?') ? '&' : '?') + 'bd_vid=' + encodeURIComponent(bdVid)
+      }
+
+      if (kValue) {
+        newUrl += (newUrl.includes('?') ? '&' : '?') + 'k=' + encodeURIComponent(kValue)
+      }
+
+      newUrl += (newUrl.includes('?') ? '&' : '?') + 's=bd-sealos-marketing-appstore'
+
+      window.open(newUrl, '_self')
+    })
+
+  })
+})
